5e59f062e2df76b37e01749a52e2c50e709b3956
Ritish Ranjan Khan <ritishranjankhan@gmail.com>
2021-02-18 03:08:56 +0530

    Documented the file

--- superSimpleStocksAssignment.py
+++ superSimpleStocksAssignment.py
@@ -1,3 +1,9 @@
+"""
+Introduction: Implementation of Super Simple Stocks system of The Global Beverage Corporation Exchange, which is used to run trading.
+Assigment : JP Morgan & Chase
+Author : Ritish Ranjan Khan
+"""
+
 import pandas
 import re
 import math
@@ -9,10 +15,13 @@ class SuperSimpleStock:
         self.gbce_input_data = self._read_stock_data(csv_file)
         self.trade = pandas.DataFrame({'Stock_Symbol':[], 'timestamp':[], 'quantity':[], 'buyORsell':[], 'price':[]})
     
-    def get_dividend_yield(self, stock_name, stock_input_price):
+    def calculate_dividend_yield(self, stock_name, stock_input_price):
+        """
+        Function to calculate dividend yield
+        """
         stock_data = self.gbce_input_data[self.gbce_input_data['Stock_Symbol'] == stock_name]
         if len(stock_data['Type']) != 1:
-            raise StockException("Invalid stock_name value for {}".format(stock_name))
+            raise SuperSimpleStocksException("Invalid stock_name value for {}".format(stock_name))
 
         if list(stock_data['Type'])[0] == 'Common':
             return list(stock_data['Last_Dividend'])[0] / stock_input_price
@@ -22,16 +31,23 @@ class SuperSimpleStock:
             raise Exception('Invalid stock_name type: {}'.format(stock_data['Type']))
 
 
-
-    def get_pe_ratio(self, stock_name, stock_input_price):
-        dividend = self.get_dividend_yield(stock_name, stock_input_price)
+    def calculate_pe_ratio(self, stock_name, stock_input_price):
+        """
+        Function to calculate the P/E Ratio
+        """
+        dividend = self.calculate_dividend_yield(stock_name, stock_input_price)
         if dividend == 0:
             return float('nan')
         return stock_input_price / dividend
 
+
     def record_trade(self, stock_name, quantity, buyORsell, price):
+        """
+        Function to record a trade, with timestamp, quantity, 
+        buy or sell indicator and price
+        """
         if stock_name not in set(self.gbce_input_data['Stock_Symbol']):
-            raise StockException("Invalid SuperSimpleStock: %s".format(stock_name))
+            raise SuperSimpleStocksException("Invalid SuperSimpleStock: %s".format(stock_name))
 
         trade = {'Stock_Symbol': stock_name,
         'timestamp': datetime.datetime.now().isoformat(),
@@ -42,7 +58,11 @@ class SuperSimpleStock:
         self.trade = self.trade.append(trade, ignore_index=True)
 
 
-    def get_valume_weighted_stock_price(self, stock_name, last_minutes=5):
+    def calculate_valume_weighted_stock_price(self, stock_name, last_minutes=5):
+        """
+        Function to calculate Volume Weighted Stock Price 
+        based on trades in past 5 minutes
+        """
         transaction = datetime.datetime.now() - datetime.timedelta(minutes=last_minutes)
 
         if len(self.trade) == 0:
@@ -61,7 +81,11 @@ class SuperSimpleStock:
         return sum(transact_stock['price'] * transact_stock['quantity']) / sum(transact_stock['quantity'])
         
 
-    def get_all_share_index(self):
+    def calculate_all_gbce_share_index(self):
+        """
+        Function to calculate the GBCE All Share Index using the geometric mean of the 
+        Volume Weighted Stock Pricefor all stocks
+        """
         if len(self.trade) == 0:
             return float('nan')
         
@@ -73,6 +97,9 @@ class SuperSimpleStock:
         return all_share_index ** (1 / n)
 
     def _read_stock_data(self, csv_file):
+        """
+        Function to read the csv file and returning a clean dataframe
+        """
         gbce_input_data = pandas.read_csv(csv_file)
         fixed_dividend = []
         for x in gbce_input_data['Fixed_Dividend']:
@@ -87,6 +114,6 @@ class SuperSimpleStock:
         gbce_input_data['Fixed_Dividend'] = fixed_dividend
         return gbce_input_data
 
-class StockException(Exception):
+class SuperSimpleStocksException(Exception):
     pass
 
--- test/superSimpleStocksAssignmentTest.py
+++ test/superSimpleStocksAssignmentTest.py
@@ -6,16 +6,16 @@ import time
 class TestSuperSimpleStocksAssignment(unittest.TestCase):
     def testCalculateDividendYield(self):
         stock_name = superSimpleStocksAssignment.SuperSimpleStock('test/stock_data.csv')
-        self.assertAlmostEqual(0, stock_name.get_dividend_yield(stock_name='TEA', stock_input_price=2))
-        self.assertAlmostEqual(7.667, stock_name.get_dividend_yield(stock_name= 'ALE', stock_input_price= 3), places= 3)
-        self.assertAlmostEqual(0.667, stock_name.get_dividend_yield(stock_name= 'GIN', stock_input_price= 3), places= 3)
+        self.assertAlmostEqual(0, stock_name.calculate_dividend_yield(stock_name='TEA', stock_input_price=2))
+        self.assertAlmostEqual(7.667, stock_name.calculate_dividend_yield(stock_name= 'ALE', stock_input_price= 3), places= 3)
+        self.assertAlmostEqual(0.667, stock_name.calculate_dividend_yield(stock_name= 'GIN', stock_input_price= 3), places= 3)
 
-        self.assertRaises(superSimpleStocksAssignment.StockException, stock_name.get_dividend_yield, stock_name= 'FOOBAR', stock_input_price= 3)
+        self.assertRaises(superSimpleStocksAssignment.SuperSimpleStocksException, stock_name.calculate_dividend_yield, stock_name= 'FOOBAR', stock_input_price= 3)
     
     def testCalculatePERatio(self):
         stock_name = superSimpleStocksAssignment.SuperSimpleStock('test/stock_data.csv')
-        self.assertTrue(math.isnan(stock_name.get_pe_ratio(stock_name='TEA', stock_input_price=2)))
-        self.assertAlmostEqual(3/7.667, stock_name.get_pe_ratio(stock_name='ALE', stock_input_price=3), places=3)
+        self.assertTrue(math.isnan(stock_name.calculate_pe_ratio(stock_name='TEA', stock_input_price=2)))
+        self.assertAlmostEqual(3/7.667, stock_name.calculate_pe_ratio(stock_name='ALE', stock_input_price=3), places=3)
 
     def testRecordTrade(self):
         stock_name = superSimpleStocksAssignment.SuperSimpleStock('test/stock_data.csv')
@@ -24,34 +24,34 @@ class TestSuperSimpleStocksAssignment(unittest.TestCase):
         stock_name.record_trade(stock_name='TEA', quantity=10, buyORsell=True, price=5)
         self.assertEqual(2, len(stock_name.trade))
 
-        self.assertRaises(superSimpleStocksAssignment.StockException, stock_name.record_trade, stock_name= 'FOOBAR', quantity= 10, buyORsell= True, price= 5)
+        self.assertRaises(superSimpleStocksAssignment.SuperSimpleStocksException, stock_name.record_trade, stock_name= 'FOOBAR', quantity= 10, buyORsell= True, price= 5)
 
     def testCalculateStockPrice(self):
         stock_name = superSimpleStocksAssignment.SuperSimpleStock('test/stock_data.csv')
-        self.assertTrue(math.isnan(stock_name.get_valume_weighted_stock_price('ALE')))
+        self.assertTrue(math.isnan(stock_name.calculate_valume_weighted_stock_price('ALE')))
         stock_name.record_trade(stock_name='ALE', quantity=10, buyORsell=True, price=5)
         stock_name.record_trade(stock_name='ALE', quantity=15, buyORsell=True, price=4)
 
-        self.assertTrue(math.isnan(stock_name.get_valume_weighted_stock_price('TEA')))
+        self.assertTrue(math.isnan(stock_name.calculate_valume_weighted_stock_price('TEA')))
         stock_name.record_trade(stock_name='TEA', quantity=100, buyORsell=True, price=50)
         stock_name.record_trade(stock_name='TEA', quantity=150, buyORsell=True, price=40)
         
-        self.assertAlmostEqual(4.4, stock_name.get_valume_weighted_stock_price('ALE'))
-        self.assertAlmostEqual(44.0, stock_name.get_valume_weighted_stock_price('TEA'))
+        self.assertAlmostEqual(4.4, stock_name.calculate_valume_weighted_stock_price('ALE'))
+        self.assertAlmostEqual(44.0, stock_name.calculate_valume_weighted_stock_price('TEA'))
 
         time.sleep(2)
-        self.assertTrue(math.isnan(stock_name.get_valume_weighted_stock_price('TEA', last_minutes=0.01)))
+        self.assertTrue(math.isnan(stock_name.calculate_valume_weighted_stock_price('TEA', last_minutes=0.01)))
 
     
     def testGetAllShareIndex(self):
         stock_name = superSimpleStocksAssignment.SuperSimpleStock('test/stock_data.csv')
-        self.assertTrue(math.isnan(stock_name.get_all_share_index()))
+        self.assertTrue(math.isnan(stock_name.calculate_all_gbce_share_index()))
         stock_name.record_trade(stock_name='ALE', quantity=10, buyORsell=True, price=1)
         stock_name.record_trade(stock_name='ALE', quantity=15, buyORsell=True, price=2)
         stock_name.record_trade(stock_name='TEA', quantity=100, buyORsell=True, price=3)
         stock_name.record_trade(stock_name='TEA', quantity=150, buyORsell=True, price=4)
 
-        self.assertAlmostEqual(2.21, stock_name.get_all_share_index(), places=2)
+        self.assertAlmostEqual(2.21, stock_name.calculate_all_gbce_share_index(), places=2)
 
 if __name__=='__main__':
     unittest.main()
